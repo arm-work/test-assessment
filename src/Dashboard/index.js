9f229ac49004553c5469a2d9be2b1c876635cb89
arm-work <armshahid.tech@gmail.com>
2021-05-17 05:51:01 -0400

    added basic structure

---
+++ src/Dashboard/index.js
@@ -0,0 +1,9 @@
+import * as React from "react";
+import { Card, CardContent, CardHeader } from '@material-ui/core';
+
+export default () => (
+    <Card>
+        <CardHeader title="Welcome to the test project" />
+        <CardContent>A Small React SPA implementation for code review </CardContent>
+    </Card>
+);
